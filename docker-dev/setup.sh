62b5d8148b956da461627ba25c9ae80c7a176f4d
Liangsheng Yin <lsyincs@gmail.com>
2026-01-27 11:22:39 -0800

    update with setup scripts and ib devices list

--- docker-dev/setup.sh
+++ docker-dev/setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+# install apt packages
+apt update
+apt install -y neovim
+
 # pip install some utilities
 pip install nvitop
 
 ln -sf /host_home/common_sync/sglang /root/sglang
+ln -sf /host_home/common_sync/my-toolbox /root/my-toolbox
 
 cd /root/sglang/python
 pip install -e . --config-settings editable_mode=compat
