1d1d619949bc6484c3c07c4b97ff572ac835d3c8
Liangsheng Yin <lsyincs@gmail.com>
2026-02-10 11:51:38 -0800

    fine-tune the setup scripts

--- src/my_toolbox/docker_dev/setup.sh
+++ src/my_toolbox/docker_dev/setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 set -e
 
-# install apt packages
+# apt utils
 apt update
 apt install -y neovim
 
-# pip install some utilities
+# pip utils
 pip install nvitop gpustat
 
+# link common sync dirs
 ln -sf /host_home/common_sync/sglang /root/sglang
 ln -sf /host_home/common_sync/my-toolbox /root/my-toolbox
+
+cd /root/sglang/python && pip install -e . --config-settings editable_mode=compat
+cd /root/my-toolbox && pip install -e . --config-settings editable_mode=compat
 
 # setup tmux
 echo "set -g mouse on" >> /root/.tmux.conf
 echo "setw -g mode-keys vi" >> /root/.tmux.conf
 echo "set -g history-limit 100000" >> /root/.tmux.conf
-
-cd /root/sglang/python
-pip install -e . --config-settings editable_mode=compat
-
 
 # setup vimrc
 VIM_DIR="/host_home/common_sync/my-toolbox/src/my_toolbox/vim"
